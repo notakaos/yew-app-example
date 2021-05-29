79c575752922bf9b0eb1c88ca4b5df83bd1e9a98
Nobutaka OSHIRO <3583286+notakaos@users.noreply.github.com>
2021-05-29 21:57:07 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
+
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self { link, value: 0 }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => {
+                self.value += 2;
+                true
+            }
+        }
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+2" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
+}
